0b98a55058bbe52a36ef42fb2c9dc76973f04b3c
zachdixo <zachdixo@linuxvdi-f26-46.ece.iastate.edu>
2026-09-14 23:21:28 -0500

    Completed Steady State MAC

--- cpre487-587-lab3-main/staged_mac/hdl/staged_mac.vhd
+++ cpre487-587-lab3-main/staged_mac/hdl/staged_mac.vhd
@@ -60,7 +60,7 @@ architecture behavioral of staged_mac is
     signal i_B: std_logic_vector(C_DATA_WIDTH -1 downto 0);
     signal sum : std_logic_vector(31 downto 0);
     signal o_Accumulator : std_logic_vector(C_OUTPUT_WIDTH - 1 downto 0);
-    
+    signal s_hold_TID : std_logic_vector(C_DATA_WIDTH - 1 downto 0);
 	
 	-- Mac state
     type STATE_TYPE is (WAIT_FOR_VALUES, HAVE_VALUES);
@@ -92,40 +92,85 @@ begin
       if ARESETN = '0' then  -- Reset
         o_Accumulator <= (others => '0');
         state       <= WAIT_FOR_VALUES;
+        s_hold_TID <= (others => '0');
 
       else
         case state is  -- State
             -- Wait here until we receive values
             when WAIT_FOR_VALUES =>
                 -- Wait here until we recieve valid values
-                if(SD_AXIS_TVALID = '1') then
-                    state <= HAVE_VALUES;
-                end if;
-			
-			-- Other stages go here	
-			when HAVE_VALUES =>
+
+                -- valid data
                 
                 if(SD_AXIS_TVALID = '1') then
-                    if(SD_AXIS_TLAST = '1') then
-                        SD_AXIS_TREADY <= '1';
-                        o_Accumulator <= (others => '0');
-                        MO_AXIS_TDATA <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH) + unsigned(o_Accumulator));
-                        state <= WAIT_FOR_VALUES;
-                        MO_AXIS_TVALID <= '0';
-
+                    -- directly input to the accumulator
+                    if(SD_AXIS_TUSER = '1') then
+                        o_Accumulator <= std_logic_vector(resize(unsigned (i_B), C_OUTPUT_WIDTH));
+                        MO_AXIS_TDATA <= std_logic_vector(resize(unsigned (i_B), C_OUTPUT_WIDTH));
+                    -- feed in as normal
                     else
-                        o_Accumulator <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH) + unsigned(o_Accumulator));
-                        MO_AXIS_TDATA <= o_Accumulator;
-                        SD_AXIS_TREADY <= '0';
-                        MO_AXIS_TVALID <= '1';
-
+                        o_Accumulator <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH));
+                        MO_AXIS_TDATA <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH));     
                     end if;
 
+                    -- set TID
+                    s_hold_TID <= SD_AXIS_TID;
 
+                    -- last segment of data
+                    if(SD_AXIS_TLAST = '1') then 
+                        SD_AXIS_TREADY <= '1';
+                        state <= WAIT_FOR_VALUES;
+                        MO_AXIS_TVALID <= '1';
+                        MO_AXIS_TID <= SD_AXIS_TID;
+                        MO_AXIS_TLAST <= '1';
+                    else
+                    -- normal flow into HAVE_VALUES state
+                        MO_AXIS_TLAST <= '0';
+                        state <= HAVE_VALUES;
+                        MO_AXIS_TVALID <= '0';
+                    end if;
 
+                -- non valid data
+                else
+                    o_Accumulator <= (others => '0');
                 end if;
-                
-                
+            
+
+
+            -- we have values 
+			when HAVE_VALUES =>
+                -- valid data
+                if(SD_AXIS_TVALID = '1') then
+
+                    -- last piec of data
+                    if (SD_AXIS_TLAST = '1') then
+                        
+                        SD_AXIS_TREADY <= '1';
+                        MO_AXIS_TDATA <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH) + unsigned(o_Accumulator));
+                        o_Accumulator <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH) + unsigned(o_Accumulator));
+                        state <= WAIT_FOR_VALUES;
+                        MO_AXIS_TVALID <= '0';
+                        MO_AXIS_TID <= s_hold_TID;
+                        MO_AXIS_TLAST <= '1';
+
+                    else
+                        -- input into Accumulator
+                        if(SD_AXIS_TUSER = '1') then
+                            o_Accumulator <= std_logic_vector(resize(unsigned (i_B), C_OUTPUT_WIDTH));
+                            MO_AXIS_TDATA <= o_Accumulator;
+                            SD_AXIS_TREADY <= '1';
+                            MO_AXIS_TVALID <= '0';
+                        -- normal input into accumulator
+                        else
+                            o_Accumulator <= std_logic_vector(resize(unsigned(i_A) * unsigned(i_B), C_OUTPUT_WIDTH) + unsigned(o_Accumulator));
+                            MO_AXIS_TDATA <= o_Accumulator;
+                            SD_AXIS_TREADY <= '1';
+                            MO_AXIS_TVALID <= '0';
+                        end if;
+
+                    end if;
+                end if;
+
             when others =>
                 state <= WAIT_FOR_VALUES;
                 -- Not really important, this case should never happen
